f079f945e930755da88d858b3c39c5eb78c96796
MSI\moneo <jvr.moneo@gmail.com>
2021-02-23 18:24:24 +0100

    Fin sección 17 - GetMedicoByID

--- routes/medicos.js
+++ routes/medicos.js
@@ -21,12 +21,13 @@ const {
     getMedicos,
     crearMedico,
     actualizarMedico,
-    borrarMedico
+    borrarMedico,
+    getMedicoById
 } = require('../controllers/medicos');
 
 const router = Router();
 
-router.get('/', getMedicos);
+router.get('/', validarJWT, getMedicos);
 
 router.post('/',
     [
@@ -46,7 +47,12 @@ router.put('/:id', [
     actualizarMedico);
 
 router.delete('/:id',
+    validarJWT,
     borrarMedico);
+
+router.get('/:id',
+    validarJWT,
+    getMedicoById);
 
 
 
